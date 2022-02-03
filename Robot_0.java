a263994c07aecbd44ed179549f335dc0ae5ed3f9
NJOMANI WONKAM BRANDON RUDEL <brandonmhdrudel@gmail.com>
2022-02-03 13:42:51 +0100

    Creation de la classe Robot et du constructeur Robot

---
+++ Robot_0.java
@@ -0,0 +1,117 @@
+package cmr.nouveau.combat;
+
+public class Robot_0 {
+	
+	private String nom;
+	private int vie;
+	private int attack;
+	
+	public Robot_0(String nom, int vie, int attack){
+		this.nom = nom;
+		this.vie = vie;
+		this.attack = attack;
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	private String name;
+	private int attack;
+	private int health;
+	
+	public Robot(String name, int attack, int health){
+		this.name = name;
+		this.attack = attack;
+		this.health = health;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	// Methode pour retirer les points de vie d'un joueur
+	public void domage(int domage){
+		
+		this.health -= domage;
+		
+		System.out.println(name + " vient de subir " + domage + " point de dégat");
+		
+		
+	}
+	
+	
+	//////////////////////////////////////////////////
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	 */
+	 
+	
+	
+}
